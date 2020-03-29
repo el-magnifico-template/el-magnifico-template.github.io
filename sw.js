021b0519ba3cb3a7583148f0d01aa93853c0b93c
el-magnifico-template <ismailezzaki97@gmail.com>
2020-03-29 13:33:56 +0100

    ha li bedelt

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@
 ---
 // importScripts('/cache-polyfill.js');
 
+const offlinePageUrl = '/offline.html'
 const siteCacheName = '{{ site.title | replace: " ", "-"}}v1';
 const dependenciesUrls = [
   "/favicon.ico",
@@ -33,14 +34,61 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.open(siteCacheName).then(async (cache) => {
-      return cache.match(event.request).then((response) => {
-        return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
- 
+    caches
+    .open(siteCacheName)
+    .then((cache) => (
+      cache
+      .match(event.request)
+      .then((cacheResponse) => {
+        return cacheResponse || fetch(event.request)
+          .then((fetchResponse) => {
+            cache.put(event.request, fetchResponse.clone())
+            return fetchResponse
+          })
+      })
+      .catch(() => {
+        if (event.request.mode === 'navigate' ||  
+            (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))
+        ) {
+          return caches.match(offlinePageUrl)
+        }
+      })
+    ))
+  )  
+})
+
+
+
+
+
+
+
+
+self.addEventListener('message', (event) => {
+  const isARefresh = (event) => event.data.message === 'refresh'
+
+  const createDeleteOperationFor = (url, siteCache, requests) => siteCache
+    .delete(requests
+    .find((request) => request.url === url))
+
+  const createDeleteOperationsForImages = (siteCache, requests) => requests
+    .filter((request) => request.url.endsWith('.jpg') && request.url.includes('posts'))
+    .map((request) => siteCache.delete(request))
+
+  const sendRefreshCompletedMessageToClient = (event) => event.ports[0].postMessage({refreshCompleted: true})
+
+  if (isARefresh(event)) {
+    caches.open(siteCacheName).then((siteCache) => {
+      siteCache.keys().then((requests) => {
+        const deleteRequestToBeRefreshed = createDeleteOperationFor(event.data.url, siteCache, requests)
+        const deleteRequestsForImagesToBeRefreshed = createDeleteOperationsForImages(siteCache, requests)
+        Promise.all([
+          deleteRequestToBeRefreshed, 
+          ...deleteRequestsForImagesToBeRefreshed
+        ])
+          .then(() => sendRefreshCompletedMessageToClient(event))
+          .catch(() => sendRefreshCompletedMessageToClient(event))
+      })
+    }) 
+  }
+})
