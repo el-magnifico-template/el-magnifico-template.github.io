b6537437895600e261912854c74fc09ef616038f
el-magnifico-template <ismailezzaki97@gmail.com>
2020-03-29 13:30:00 +0100

    ha li bedelt

--- sw.js
+++ sw.js
@@ -43,36 +43,4 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
-
-
-
-// strategies from the offline cookbook by jake archibald
-// https://jakearchibald.com/2014/offline-cookbook/#serving-suggestions-responding-to-requests
-
-
-
-  // Promise.race is no good to us because it rejects if
-  // a promise rejects before fulfilling. Let's make a proper
-  // race function:
-  function promiseAny(promises) {
-    return new Promise((resolve, reject) => {
-      // make sure promises are all promises
-      promises = promises.map(p => Promise.resolve(p));
-      // resolve this promise as soon as one resolves
-      promises.forEach(p => p.then(resolve));
-      // reject if all promises reject
-      promises.reduce((a, b) => a.catch(() => b))
-        .catch(() => reject(Error("All failed")));
-    });
-  };
-
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      promiseAny([
-        caches.match(event.request),
-        fetch(event.request)
-      ])
-    );
-  });
-
+ 
