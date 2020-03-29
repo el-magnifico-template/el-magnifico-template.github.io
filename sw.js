7e402c03cfb4c46a3636ef8b2b2811fb0ed09876
el-magnifico-template <ismailezzaki97@gmail.com>
2020-03-29 12:50:58 +0100

    ha li bedelt

---
+++ sw.js
@@ -0,0 +1,45 @@
+---
+---
+importScripts('/cache-polyfill.js');
+
+const siteCacheName = 'mysiteCachev11';
+const dependenciesUrls = [
+  "/favicon.ico",
+   "offline.html",
+   
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(siteCacheName).then((cache) => {
+      return cache.addAll(dependenciesUrls);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== siteCacheName
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(siteCacheName).then(async (cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
